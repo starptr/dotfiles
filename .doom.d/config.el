9cc74c00aa9d54776fe9640d8568b3571651e4e5
Yuto Nishida <starptr@users.noreply.github.com>
2021-08-15 14:19:06 -0700

    Config doom cc

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -73,3 +73,12 @@
 
 ; Location of projects
 (setq projectile-project-search-path '("~/src" "~/Documents/GitHub"))
+
+; Set clangd args
+(setq lsp-clients-clangd-args '("-j=3"
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"
+                                "--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))

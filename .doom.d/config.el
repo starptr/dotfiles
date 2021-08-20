a1ccfdf01c45ef29c5b0f08a610da87ab6cf94b4
Yuto Nishida <starptr@users.noreply.github.com>
2021-08-19 22:45:58 -0700

    Config doom theme toggle function

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,7 +25,12 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-one)
+(defun toggle-theme ()
+  (if (eq (symbol-value 'doom-theme) 'doom-one)
+      (load-theme 'doom-one-light t)
+    (load-theme 'doom-one t)))
+(evil-ex-define-cmd "theme" #'toggle-theme)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!

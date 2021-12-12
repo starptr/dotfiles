67029589485ceb822f2bd37f69942a035ef3c772
Yuto Nishida <starptr@users.noreply.github.com>
2021-12-12 04:05:27 -0800

    Add lvim debug keybinds

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -214,6 +214,18 @@ lvim.plugins = {
 
 
 -- Neovim keybinds
+local dap = require('dap')
+vim.api.nvim_set_keymap('n', '<F5>', [[<Cmd>lua require'dap'.continue()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F3>', [[<Cmd>lua require'dap'.terminate()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F4>', [[<Cmd>lua require'dap'.terminate(); require'dap'.run_last()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F6>', [[<Cmd>lua require'dap'.pause()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', [[<Cmd>lua require'dap'.toggle_breakpoint()<CR>]], { noremap = true, silent = true })
+-- TODO: conditional breakpoints
+vim.api.nvim_set_keymap('n', '<F8>', [[<Cmd>lua require'dap'.run_to_cursor()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F10>', [[<Cmd>lua require'dap'.step_over()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua require'dap'.step_into()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F12>', [[<Cmd>lua require'dap'.step_out()<CR>]], { noremap = true, silent = true })
+-- TODO: more debug commands https://github.com/mfussenegger/nvim-dap/blob/9b8c27d6dcc21b69834fe9c2d344e49030783390/doc/dap.txt#L460
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {

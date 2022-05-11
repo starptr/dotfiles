50b11f7018b1f7dfa7e1b9dde9d1a1b30604b9b9
Yuto Nishida <yuto@berkeley.edu>
2022-05-10 17:12:26 -0700

    Config oscyank and gitlinker

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -205,10 +205,37 @@ lvim.lsp.automatic_servers_installation = true
 
 -- Additional Plugins
 lvim.plugins = {
-  {'stevearc/dressing.nvim'},
+  {
+    'stevearc/dressing.nvim',
+    config = function ()
+      require('dressing').setup({
+        input = {
+          override = function (conf)
+            conf['row'] = 2
+            conf['col'] = -1
+          end,
+        },
+      })
+    end,
+  },
   {
     'Pocco81/dap-buddy.nvim',
     commit = "24923c3819a450a772bb8f675926d530e829665f",
+    config = function ()
+      -- TODO: use legendary
+      --local dap = require('dap')
+      vim.api.nvim_set_keymap('n', '<F5>', [[<Cmd>lua require'dap'.continue()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F3>', [[<Cmd>lua require'dap'.terminate()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F4>', [[<Cmd>lua require'dap'.terminate(); require'dap'.run_last()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F6>', [[<Cmd>lua require'dap'.pause()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F9>', [[<Cmd>lua require'dap'.toggle_breakpoint()<CR>]], { noremap = true, silent = true })
+      -- TODO: conditional breakpoints
+      vim.api.nvim_set_keymap('n', '<F8>', [[<Cmd>lua require'dap'.run_to_cursor()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F10>', [[<Cmd>lua require'dap'.step_over()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua require'dap'.step_into()<CR>]], { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<F12>', [[<Cmd>lua require'dap'.step_out()<CR>]], { noremap = true, silent = true })
+      -- TODO: more debug commands https://github.com/mfussenegger/nvim-dap/blob/9b8c27d6dcc21b69834fe9c2d344e49030783390/doc/dap.txt#L460
+    end,
   },
   {
     "mrjones2014/legendary.nvim",
@@ -272,23 +299,47 @@ lvim.plugins = {
   {
     "ruifm/gitlinker.nvim",
     requires = 'nvim-lua/plenary.nvim',
+    setup = function ()
+      require('legendary').bind_keymap({
+        'gh',
+        {
+          n = { function() require'gitlinker'.get_repo_url() end, description = "Remote repo url" },
+          v = { function() require'gitlinker'.get_buf_range_url('v', {}) end, description = "Remote repo url with lines" },
+        },
+      })
+    end,
+    config = function ()
+      require('gitlinker').setup({
+        mappings = nil,
+        opts = {
+          action_callback = function (url)
+            vim.api.nvim_command('let @" = \'' .. url .. '\'')
+            vim.fn.setreg('+', url)
+            vim.api.nvim_command(':OSCYankReg +')
+          end,
+        }
+      })
+    end,
+  },
+  {
+    "ojroques/vim-oscyank",
+    branch = "main",
+    cmd = { "OSCYank", "OSCYankReg" },
+    setup = function ()
+      require('legendary').bind_keymap({
+        'gy',
+        {
+          n = {':OSCYank<CR>', opts = { noremap = true }},
+          v = {':OSCYank<CR>', opts = { noremap = true }},
+        },
+        description = 'Copy with OSC52',
+      })
+    end,
   }
 }
 
 
 -- Neovim keybinds
-local dap = require('dap')
-vim.api.nvim_set_keymap('n', '<F5>', [[<Cmd>lua require'dap'.continue()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F3>', [[<Cmd>lua require'dap'.terminate()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F4>', [[<Cmd>lua require'dap'.terminate(); require'dap'.run_last()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F6>', [[<Cmd>lua require'dap'.pause()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F9>', [[<Cmd>lua require'dap'.toggle_breakpoint()<CR>]], { noremap = true, silent = true })
--- TODO: conditional breakpoints
-vim.api.nvim_set_keymap('n', '<F8>', [[<Cmd>lua require'dap'.run_to_cursor()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F10>', [[<Cmd>lua require'dap'.step_over()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F11>', [[<Cmd>lua require'dap'.step_into()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F12>', [[<Cmd>lua require'dap'.step_out()<CR>]], { noremap = true, silent = true })
--- TODO: more debug commands https://github.com/mfussenegger/nvim-dap/blob/9b8c27d6dcc21b69834fe9c2d344e49030783390/doc/dap.txt#L460
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
